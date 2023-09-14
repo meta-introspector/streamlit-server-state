2d74ae904cc9ebb25f264f8e52e4695e7f2bd34e
mike dupont <mike.dupont@introspector.local>
2023-09-14 11:46:51 -0400

    this version allows for selecting and importing
    you can select what you want to share and then share the url and they
    can import it.

--- app_chat.py
+++ app_chat.py
@@ -1,14 +1,35 @@
 import streamlit as st
 import streamlit.components.v1 as components
 from streamlit_server_state import server_state, server_state_lock
-
+import urllib.parse        
     
 nickname = st.text_input("Nick name", key="nickname")
 
 q= st.experimental_get_query_params()
 if "nickname" in q:
     nickname = q["nickname"][0]
-    
+new_messages = []                
+if "messages" in q:
+    for item in q["messages"]:
+        new1 = urllib.parse.unquote(item)
+        new_messages.append(new1)
+for m in new_messages:    
+    st.write("m", m )
+    #chk = st.checkbox(f"include {text}",key=str("_i_"+str(i)))
+    i = str(id(m))
+    chk = st.checkbox(f"approve {m}",key=str("_a_"+str(i)),value=True)
+    text = st.text_input("approve", key=str("_u_"+str(i)),value=m)
+
+    # number_input("Include" +m,
+    #              min_value=0, max_value=10, value=1,
+    #              step=1,
+    #              #format=None,
+    #              key="count"+m,
+    #              help=f"How may times to include {m}",
+    #              #on_change=None, args=None, kwargs=None, *, disabled=False, label_visibility="visible"
+    #              )
+
+        
 if not nickname:
     st.stop()
 
@@ -18,11 +39,40 @@ def on_clear():
     with server_state_lock["chat_messages"]:
         if "chat_messages" in server_state:
             da = server_state["chat_messages"]
+            st.write("Messages")        
             st.code(da)        
         server_state["chat_messages"]=[]
     
+clear_args = st.button("clear args")
+if clear_args:
+    st.experimental_set_query_params()
 
 st.button("clear",on_click=on_clear)
+
+imports = st.button(f"Approve selected")
+if imports:
+    messages= []
+    st.write("going to import")
+    for x in st.session_state:
+        v = st.session_state[x]
+        if x.startswith("_a_"):
+            value_name = "_u_"+x[3:]
+            if value_name in st.session_state:
+                v2 = st.session_state[value_name]
+                if v == True:
+                    #messages.append(dict(value=v2) )
+                    new_message_packet = {
+                        "src":"import",
+                        "nickname": nickname,
+                        "text": v2,
+                    }
+                    with server_state_lock["chat_messages"]:
+                        server_state["chat_messages"] = server_state["chat_messages"] + [
+                            new_message_packet
+                        ]
+                        st.write("added",new_message_packet)
+
+
 
 def on_message_input():
     new_message_text = st.session_state["message_input"]
@@ -30,13 +80,14 @@ def on_message_input():
         return
 
     new_message_packet = {
+        "src":"input",
         "nickname": nickname,
         "text": new_message_text,
     }
     
-    if new_message_text.startswith("/delete-all"):
-        print(server_state["chat_messages"])
-        server_state["chat_messages"]=[]
+    #if new_message_text.startswith("/delete-all"):
+    #    print(server_state["chat_messages"])
+    #    server_state["chat_messages"]=[]
     
     with server_state_lock["chat_messages"]:
         server_state["chat_messages"] = server_state["chat_messages"] + [
@@ -56,15 +107,19 @@ with server_state_lock["chat_messages"]:
         v = urls1[x]
         for k in v:
             #st.write(k)
-            urls.append( {"nickname": nickname, "text": k})
+            if x not in ("nickname","messages"):
+                urls.append( {
+                    "src":"url",
+                    "field":x,
+                    "nickname": nickname, "text": k})
     server_state["chat_messages"].extend(urls)           
 
 st.text_input("Message", key="message_input", on_change=on_message_input)
 
 #st.write(server_state["chat_messages"])
 
-for message in server_state["chat_messages"]:
-    st.write(message)
+for i,message in enumerate(server_state["chat_messages"]):
+    st.write("MESSAGE",message)
 
     if "text" in message:
         text = message["text"]
@@ -73,3 +128,50 @@ for message in server_state["chat_messages"]:
                                       #key=text
                                   #, width=500, height=400
                                       )
+
+        i = str(id(text))
+        chk = st.checkbox(f"include {text}",key=str("_i_"+str(i)))
+        text = st.text_input("include", key=str("_t_"+str(i)),value=text)
+    # now make a share link
+share = st.button(f"share selected")
+if share:
+    messages= []
+    st.write("going to share")
+    for x in st.session_state:
+        v = st.session_state[x]
+        
+        if x.startswith("_i_"):
+            value_name = "_t_"+x[3:]
+            #st.write("DEBUG!A",x,value_name)
+            if value_name in st.session_state:
+
+                v2 = st.session_state[value_name]
+
+                if v == True:
+                    #st.write("DEBUGA",x,value_name, v2, v)
+                    messages.append(dict(value=v2) )
+                    #{ "DEBUG1" :{ x : v2    }}
+                    
+            else:
+                #st.write("DEBUG2",x,value_name)
+                for x in st.session_state:
+                    [x,value_name]
+
+        else:
+            #st.write("DEBUG4",x,v)
+            pass
+    q.update(dict(messages=messages))
+    encoded_query = urllib.parse.urlencode(q, doseq=True)
+    st.markdown(f"* share [input_link {encoded_query}](/?{encoded_query})")
+
+ 
+# select_all = st.button("select all")
+# if select_all:
+#     for x in st.session_state:
+#         v = st.session_state[x]
+#         if x.startswith("_i_"):
+#             if v == True:
+#                 pass
+#             else:
+#                 st.session_state[x] = True
+                    
